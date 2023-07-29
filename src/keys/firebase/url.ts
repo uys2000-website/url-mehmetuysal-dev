2e438e65824a41ffa0c28b434a24a667d6a409a6
Mehmet Uysal <mehmetuysal2000@yandex.com>
2023-07-30 01:11:47 +0300

    [Release Changes]
    Keys merged partially to env and other putted in to project
    Facicon added
    Settings and gitignore files updated

---
+++ src/keys/firebase/url.ts
@@ -0,0 +1,11 @@
+// for url.mehmetuysal.dev
+
+export default {
+  apiKey: "AIzaSyDR0ZUa9Jr7Lc10E_Io0mHJg8pYJc4eehs",
+  authDomain: "url-mehmetuysal-dev.firebaseapp.com",
+  projectId: "url-mehmetuysal-dev",
+  storageBucket: "url-mehmetuysal-dev.appspot.com",
+  messagingSenderId: "57733275174",
+  appId: "1:57733275174:web:2c8859d45f27e5e4d690e7",
+  measurementId: "G-JRC94NFJ1K",
+};
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 export default {
   isDebugWithStringMode: false,
-  isDebugMode: true,
+  isDebugMode: false,
 };
